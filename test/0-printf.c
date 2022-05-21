01ba94ec6308e94906961ca2577eb8dd5f95ae63
BeccaQ <beccamiwa@gmail.com>
2022-05-21 16:17:53 +0100

    added main.h and 0-printf.c

---
+++ test/0-printf.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _printf - prints inputs to the standard output
+ * @format: Pointer to a formated string
+ * Return: number of characters printed to the screen
+ */
+int _printf(const char *format, ...)
+{
+	int count, i;
+
+	va_list f;
+
+	va_start(f, format);
+
+
+	va_end(f);
+}
