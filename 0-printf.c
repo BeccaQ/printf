d8ecba6f2d9c055c0c4298733b2c002edefd74ec
BeccaQ <beccamiwa@gmail.com>
2022-05-23 00:06:14 +0100

    moved files from test to devops

---
+++ 0-printf.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _printf - Prints output acording to an input format.
+ * @format: Pointer to a formated string
+ *
+ * Return: number of characters printed to the screen
+ */
+int _printf(const char *format, ...)
+{
+	int i;
+	char ch, ch2;
+	va_list next;
+
+	va_start(next, format);
+
+	if (format == NULL)
+		return (-1);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		ch = format[i];
+		ch2 = format[i + 1];
+
+		if (ch == '%')
+		{
+			if ((ch2 == 'd' || ch2 == 'i'))
+			{
+				print_number(va_arg(next, int));
+				i++;
+				continue;
+			}
+
+			if (ch2 == 'c')
+			{
+				_putchar(va_arg(next, int));
+				i++;
+				continue;
+			}
+
+			if (ch2 == 's')
+			{
+				_puts(va_arg(next, char *));
+				i++;
+				continue;
+			}
+		}
+
+		_putchar(ch);
+	}
+
+	va_end(next);
+
+	return (count);
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+void print_number(int n);
+void _puts(char *str);
+extern int count;
+
+#endif /* MAIN_H */
