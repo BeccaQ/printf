b1cead778e8c830aa8276eb6735f723b6418e041
BeccaQ <beccamiwa@gmail.com>
2022-05-23 02:04:04 +0100

    fixed betty error in 0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -14,15 +14,12 @@ int _printf(const char *format, ...)
 
 	va_start(next, format);
 	count = 0;
-
 	if (format == NULL)
 		return (-1);
-
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		ch = format[i];
 		ch2 = format[i + 1];
-
 		if (ch == '%')
 		{
 			if ((ch2 == 'd' || ch2 == 'i'))
@@ -30,34 +27,25 @@ int _printf(const char *format, ...)
 				print_number(va_arg(next, int));
 				i++;
 				continue;
-			}
-
-			if (ch2 == 'c')
+			} else if (ch2 == 'c')
 			{
 				_putchar(va_arg(next, int));
 				i++;
 				continue;
-			}
-
-			if (ch2 == 's')
+			} else if (ch2 == 's')
 			{
 				_puts(va_arg(next, char *));
 				i++;
 				continue;
-			}
-
-			if (ch2 == '%')
+			} else if (ch2 == '%')
 			{
 				_putchar('%');
 				i++;
 				continue;
 			}
 		}
-
 		_putchar(ch);
 	}
-
 	va_end(next);
-
 	return (count);
 }
